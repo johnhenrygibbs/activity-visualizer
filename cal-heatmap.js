fe410bfe8d7b9ec8bdd33a32461a6655a02f7011
John Gibbs <hello@johnhenrygibbs.com>
2024-02-22 14:29:25 -0800

    Options Cal-Heatmap

--- cal-heatmap.js
+++ cal-heatmap.js
@@ -1,3 +1,18 @@
 const cal = new CalHeatmap();
 
-cal.paint({});
+cal.paint({
+    date : {
+        start : new Date('2024-01-01')
+    },
+    range : 12,
+    domain : {
+        type : 'month',
+        gutter : 15,
+        dynamicDimension : true,
+    },
+    subDomain : {
+        type : 'xDay',
+        radius : 85,
+
+    }
+});
