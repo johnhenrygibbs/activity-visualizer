812e80cd0ada6b4601e2f6723b1c9b9f6adbae4c
John Gibbs <hello@johnhenrygibbs.com>
2024-02-23 21:15:40 -0800

    Trying Data

--- cal-heatmap.js
+++ cal-heatmap.js
@@ -1,8 +1,18 @@
 const cal = new CalHeatmap();
 
+const data = [
+    { date: '2024-01-01', value: 3 },
+    { date: '2024-01-02', value: 6 },
+]
+
 cal.paint({
+    verticalOrientation : false,
     date : {
         start : new Date('2024-01-01')
+    },
+    data : {
+        source : data,
+        x: 'date'
     },
     range : 12,
     domain : {
